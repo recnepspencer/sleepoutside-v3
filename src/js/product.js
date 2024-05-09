bb826e54ee1dce2c2fc179bcc613259165641601
Spencer Hepworth <goldenspencerh@gmail.com>
2024-05-09 16:02:53 -0600

    finished merging

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,7 @@
 import { setLocalStorage,getLocalStorage, setClick } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
-<<<<<<< HEAD
-import { getLocalStorage, getParam } from "./utils.mjs";
-
-//get , set , update the local storage
-function addProductToCart(product) {
-  // console.log(product);
-  let cart = [];
-  if (getLocalStorage("so-cart")) {
-      getLocalStorage("so-cart").forEach(oldCartItem => {
-        cart.push(oldCartItem);
-      });
-  }
-  // if (!Array.isArray(cart)) {
-  //   cart = [];
-  // }
-  cart.push(product);
-  // console.log(cart);
-  setLocalStorage("so-cart", cart);
-}
-=======
-import { getLocalStorage } from "./utils.mjs";
 import { getParam } from "./utils.mjs";
 import tentDetails from "./tentDetail.mjs"
->>>>>>> revert-branch
 
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -39,10 +17,5 @@ document
   .addEventListener("click", addToCartHandler);
 
 
-<<<<<<< HEAD
-// add listener to Add to Cart button
-setClick ("#addToCart", addToCartHandler); // Attach event listener
-=======
   const productId = getParam('product');
  tentDetails(productId, '.product-detail');
->>>>>>> revert-branch
--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -5,19 +5,6 @@ function convertToJson(res) {
       throw new Error("Bad Response");
   }
 }
-<<<<<<< HEAD
-  
-export function getData(category = "tents") {
-return fetch(`../json/${category}.json`)
-  .then(convertToJson)
-  .then((data) => data);
-}
-
-export async function findProductById(id) {
-const products = await getData();
-return products.find((item) => item.Id === id);
-}
-=======
 
 export function getData(category = 'tents') {
   return fetch(`../json/${category}.json`)
@@ -33,5 +20,3 @@ export async function findProductById(id, category) {
   const products = await getData(category);
   return products.find((item) => item.Id === id);
 }
-
->>>>>>> revert-branch
