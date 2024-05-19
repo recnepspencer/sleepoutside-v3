5f26b6adda0dc28233aac7ab96f3ffacf4b7bbde
mbower00 <mitchbbower@gmail.com>
2024-05-18 23:55:57 -0600

    Alerts!

---
+++ src/js/alert.mjs
@@ -0,0 +1,43 @@
+// here is an example alert that could be added to the list in alerts.json
+// {
+//     "message":"I am a test!",
+//     "background":"var(--secondary-color)",
+//     "color":"white"
+// }
+
+async function generateAlerts(){
+    let shouldGenerate = true
+
+    // used code from ./productData.mjs
+    const fetched = await fetch("../public/json/alerts.json")
+    let alertData
+    try{
+        alertData = await fetched.json()
+    } catch {
+        shouldGenerate = false
+    }
+
+    if (shouldGenerate && alertData.length == 0) {
+        shouldGenerate = false
+    }
+
+    if (shouldGenerate) {
+        const alertListElement = document.createElement("section")
+        alertListElement.classList.add("alert-list")
+
+        alertData.forEach((alert) => {
+            const alertElement = document.createElement("p")
+            
+            alertElement.textContent = alert.message
+            alertElement.style.color = alert.color
+            alertElement.style.backgroundColor = alert.background
+            
+            alertListElement.appendChild(alertElement)
+        })
+
+        document.querySelector("main").prepend(alertListElement)
+    }
+
+}
+
+generateAlerts()
