164b8873021349c1912f30d353207b421076874b
Spencer Hepworth <118311110+recnepspencer@users.noreply.github.com>
2024-05-28 16:24:24 -0600

    fixed header in one component

--- src/js/components/ProductSummary.svelte
+++ src/js/components/ProductSummary.svelte
@@ -1,26 +1,30 @@
 <script>
-  import { getParam } from "../utils.mjs";
-
-  export let pro = {};
-  export let category = getParam("category");
-  function getImageUrl(product) {
-    return product.Images.PrimaryLarge;
-  }
-
-  function applyDiscount(product) {
-    if (product.IsClearance) {
-      const discountedPrice = product.FinalPrice * 0.8;
-      return `<span class="strikethrough">${product.ListPrice}</span> $${discountedPrice.toFixed(2)}`;
+    import { getParam } from "../utils.mjs";
+  
+    export let pro = {};
+    export let category = getParam("category");
+  
+    function getImageUrl(product) {
+      return product.Images.PrimaryLarge;
     }
-    return `$${product.FinalPrice}`;
-  }
-</script>
-
-<section>
-  <a href={`/product_pages/index.html?category=${category}&product=${pro.Id}`}>
-    <img src={getImageUrl(pro)} alt={pro.NameWithoutBrand} />
-    <h3 class="card__brand">{pro.Brand.Name}</h3>
-    <h2 class="card__name">{pro.NameWithoutBrand}</h2>
-    <p class="product-card__price">{@html applyDiscount(pro)}</p>
-  </a>
-</section>
+  
+    function applyDiscount(product) {
+      let finalPrice = product.FinalPrice;
+  
+      if (product.IsClearance) {
+        finalPrice = finalPrice * 0.8;
+        return `<span class="strikethrough">$${product.ListPrice.toFixed(2)}</span> $${finalPrice.toFixed(2)}`;
+      }
+      return `$${finalPrice.toFixed(2)}`;
+    }
+  </script>
+  
+  <section>
+    <a href={`/product_pages/index.html?category=${category}&product=${pro.Id}`}>
+      <img src={getImageUrl(pro)} alt={pro.NameWithoutBrand} />
+      <h3 class="card__brand">{pro.Brand.Name}</h3>
+      <h2 class="card__name">{pro.NameWithoutBrand}</h2>
+      <p class="product-card__price">{@html applyDiscount(pro)}</p>
+    </a>
+  </section>
+  
